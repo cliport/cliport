4bfa063118e47045d5b3ce0656a63e78a1495f3c
Mohit Shridhar <mohit.appi@gmail.com>
2021-09-21 13:27:49 -0700

    updating download scripts

---
+++ scripts/quickstart_download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --no-check-certificate -r 'https://drive.google.com/uc?export=download&id=1w8yzqrIf-bTXp6NazQ_o8V-xiJB3tlli' -O cliport_quickstart.zip
+unzip cliport_quickstart.zip -d cliport_quickstart
+rm cliport_quickstart.zip
