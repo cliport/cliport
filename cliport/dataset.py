1640de941ae2d72ab95443b3b8a2526f36d71eba
Mohit Shridhar <mohit.appi@gmail.com>
2021-09-28 21:02:22 -0700

    actually just misreading. reverting

--- cliport/dataset.py
+++ cliport/dataset.py
@@ -391,15 +391,15 @@ class RavensMultiTaskDataset(RavensDataset):
         'multi-language-conditioned': {
             'train': [
                 'align-rope',
-                'assembling-kits-seq-seen-colors',
-                'packing-boxes-pairs-seen-colors',
+                'assembling-kits-seq-unseen-colors',
+                'packing-boxes-pairs-unseen-colors',
                 'packing-shapes',
-                'packing-seen-google-objects-seq',
-                'packing-seen-google-objects-group',
-                'put-block-in-bowl-seen-colors',
-                'stack-block-pyramid-seq-seen-colors',
-                'separating-piles-seen-colors',
-                'towers-of-hanoi-seq-seen-colors',
+                'packing-unseen-google-objects-seq',
+                'packing-unseen-google-objects-group',
+                'put-block-in-bowl-unseen-colors',
+                'stack-block-pyramid-seq-unseen-colors',
+                'separating-piles-unseen-colors',
+                'towers-of-hanoi-seq-unseen-colors',
             ],
             'val': [
                 'align-rope',
